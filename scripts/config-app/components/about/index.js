2999d7794ddd6fc025a9f86548664335ebeceac2
Sören Wrede <soerenwrede@gmail.com>
2018-04-04 14:44:11 +0200

    I18n: Add text domain to welcome title

--- scripts/config-app/components/about/index.js
+++ scripts/config-app/components/about/index.js
@@ -11,7 +11,7 @@ function About({ onCreateTemplate }) {
 
   return (
     <div className="gcf-about">
-      <h1>{__("Welcome to Gutenberg Custom Fields")}</h1>
+      <h1>{__("Welcome to Gutenberg Custom Fields", "gutenberg-custom-fields")}</h1>
       <p>
         {__(
           "Gutenberg Custom Fields allows you to control the content of the Gutenberg edit screen by creating pre-filled templates.",
